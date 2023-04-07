fd99e37bf71b3b9dd244fc5308dc56a05a7564fb
Montenigri <gabrielemusso@live.it>
2023-04-07 17:15:08 +0200

    .

---
+++ Riconoscimento volti/test.py
@@ -0,0 +1,53 @@
+import os
+import glob 
+import numpy as np
+import cv2
+import time
+
+
+start = time.time()
+
+root = "foto64x64"
+cwd = os.getcwd()
+listDir = os.listdir(root)
+tagFoto = {}
+
+for dir in listDir:
+    imgs =  glob.glob(f"{root}/{dir}/*.jpg")
+    tagFoto[dir] = imgs
+
+
+imgMean = np.zeros((64,64,3), dtype=np.float32)
+imgNumber = 0
+
+imgMeanPerPosition = []
+
+for key in tagFoto:
+    for im in tagFoto[key]:
+        img = cv2.imread(im).astype(np.float32)
+        imgMean += img
+        imgNumber+=1
+imgMean /= imgNumber
+
+imgMean = imgMean.astype(np.uint8)
+
+for key in tagFoto:
+    number = 0
+    newValue = []
+
+    for im in tagFoto[key]:
+        img = cv2.imread(im,0).astype(np.float32)
+        eigenvalue= np.linalg.eig(img) 
+        nv = [img, eigenvalue]
+        newValue.append(nv)
+        
+    tagFoto.update({key : newValue})
+
+#print (tagFoto)
+
+end = time.time()
+
+timeElapsed = end - start
+
+print(timeElapsed)
+
