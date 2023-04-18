569216666b62194247ee92250947d369f6ee6556
Montenigri <gabrielemusso@live.it>
2023-04-18 14:58:26 +0200

    .

--- Riconoscimento volti/test.py
+++ Riconoscimento volti/test.py
@@ -3,7 +3,8 @@ import glob
 import numpy as np
 import cv2
 from sklearn.decomposition import PCA
-
+import matplotlib.pyplot as plt
+import pickle
 
 faces_xml = 'haarcascade_frontalface_alt.xml'
 face_cascade = cv2.CascadeClassifier()
@@ -38,7 +39,7 @@ def violajones(im):
 faceMean = np.zeros((64,64), dtype=np.float32)
 faceNumber = 0
 listOfArray = []
-
+faceLabel = []
 
 for key in tagFoto:
     for im in tagFoto[key]:
@@ -46,6 +47,7 @@ for key in tagFoto:
         faceMean += vj
         flat = vj.flat
         listOfArray.append(flat)
+        faceLabel.append(key)
         faceNumber+=1
 
 faceMean /= faceNumber
@@ -54,72 +56,51 @@ faceMean /= faceNumber
 MatrixFlattenedImages = np.vstack(listOfArray)  
 flattenFaceMean = faceMean.flatten()
 
-covArray = []
-
-for key in tagFoto:
-    for im in tagFoto[key]:
-        vj = violajones(im)
-        flat = vj.flatten()
-        covArray.append(flat-flattenFaceMean)
 '''
-def covSub(_1d):
-    return _1d-flattenFaceMean
-
-covArray = np.apply_along_axis(covSub,1,MatrixFlattenedImages)
-'''
-
-def check_symmetric(a, rtol=1e-05, atol=1e-08):
-    return np.allclose(a, a.T, rtol=rtol, atol=atol)
-
-covArray = np.array(covArray)
-cov = np.dot(np.transpose(covArray),covArray)
-cov /= faceNumber
-
-pca = PCA().fit(cov)
-n_components = 50
-eigenfaces = pca.components_[:n_components]
-
-for i in range(16):
-    cv2.imshow(f"{i} autofaccia",eigenfaces[i].reshape(64,64))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-#cov = np.cov(MatrixFlattenedImages, faceMean)
-
-#cv2.imshow("viola" , faceMean)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-#print(cov)
-
-
-
-
-'''
-for key in tagFoto:
-    newValue = []
-    for im in tagFoto[key]:
-        img = cv2.imread(im,0).astype(np.float32)
-        eigenvalue= np.linalg.eig(img) 
-        nv = [img, eigenvalue]
-        newValue.append(nv)
-    tagFoto.update({key : newValue})
-
-
-
-listOfArray = []
-for key in tagFoto:
-    for im in tagFoto[key]:
-        flat = cv2.imread(im,0).flatten()
-        listOfArray.append(flat)
-
-MatrixFlattenedImages = np.vstack(listOfArray)  
-
 pca = PCA().fit(MatrixFlattenedImages)
 
-n_components = 2000
-eigenfaces = pca.components_[:n_components]
+
+with open('pca.pkl', 'wb') as pickle_file:
+        pickle.dump(pca, pickle_file)
 
 '''
 
+with open('pca.pkl', 'rb') as pickle_file:
+    pca = pickle.load(pickle_file)
+
+n_components = 4096
+eigenfaces = pca.components_[:n_components]
+ 
+# Show the first 16 eigenfaces
+#fig, axes = plt.subplots(4,4,sharex=True,sharey=True,figsize=(8,10))
+#for i in range(16):
+#    axes[i%4][i//4].imshow(eigenfaces[i].reshape(64,64), cmap="gray")
+#plt.show()
+
+
+if "Gabriele_Musso" in faceLabel:
+  print("Element is in the list")
+else:
+  print("Element is not in the list")
+
+weights = eigenfaces @ (MatrixFlattenedImages - pca.mean_).T
+
+vj = violajones("test/0.jpg")
+query = vj.reshape(1,-1)
+query_weight = eigenfaces @ (query - pca.mean_).T
+euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
+best_match = np.argmin(euclidean_distance)
+print("Best match %s with Euclidean distance %f" % (faceLabel[best_match], euclidean_distance[best_match]))
+# Visualize
+fig, axes = plt.subplots(1,2,sharex=True,sharey=True,figsize=(8,6))
+axes[0].imshow(query.reshape(64,64), cmap="gray")
+axes[0].set_title("Query")
+axes[1].imshow(MatrixFlattenedImages[best_match].reshape(64,64), cmap="gray")
+axes[1].set_title(f"{faceLabel[best_match]} distanza: {euclidean_distance[best_match]}")
+plt.show()
+
+
+'''
+Training violajons con una sola foto, poi ricopio la funzione ed aggiungo un array da tornare
+a quel punto cerco il best match
+'''
