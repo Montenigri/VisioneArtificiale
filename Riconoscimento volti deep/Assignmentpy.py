9f83e24c5ff30c79a7a86f29056a1104f6dee263
Montenigri <gabrielemusso@live.it>
2023-05-31 09:45:40 +0000

    .

---
+++ Riconoscimento volti deep/Assignmentpy.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.models import Sequential
+from keras.layers import Conv2D, Flatten, Dense
+from keras.utils import to_categorical
+
+import sys
+sys.path.append("/yoloface/")
+
+
+
